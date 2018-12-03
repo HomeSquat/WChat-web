c074dd22e1c1b10b199fceccd03b667b922b403a
dongdongjie <384168902@qq.com>
2018-12-03 19:35:34 +0800

    vue-cli配置文件（初始版）

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  baseUrl: process.env.NODE_ENV === "production" ? "./" : "/", // 相当于webpack的output.publicPath
+  outputDir: "./lala", // 相当于webpack的output.path
+  assetsDir: "/s", // 打包生成的静态资源
+  indexPath: "index.html", // 打包输出路径（相对于outputDir）
+  filenameHashing: true, // 生成的静态文件名是否有哈希值
+  lintOnSave: true, // eslint错误显示形式 boolean | 'error' ，'error'输出到浏览器遮罩中，true输出到控制台中，false不输出
+  transpileDependencies: [] // Babel显式转换依赖关系的文件
+  // configureWebpack: (config) => {
+  //   config.module
+  //     .plugins('');
+  // }, // webpack配置（基础配置）
+  // chainWebpack: (config) => {
+  //   config.module
+  //     .rule('vue')
+  //     .use('vue-loader')
+  //     .loader('vue-loader')
+  //     .tap(options => options);
+  // }, // 维护内部webpack配置,能够定义命名加载器规则和命名插件
+};
